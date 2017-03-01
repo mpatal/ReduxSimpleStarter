2e42eea65df20188cb955dc2c3cf6e744cef86c9
Marvin <marvin.patal@hotmail.com>
2017-02-28 21:05:57 -0500

    Implement video thumbnails

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import chalk from 'chalk';
 import SearchBar from './components/search_bar'; //files we write needs a relative path directory
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBvW9Etj1HtlKwwWUoS_AsuqqjNiYdw_go';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos}/> 
             </div>
         );
     }
